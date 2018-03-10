6317efccbec86be2d3d87adedb0f7ac18e433527
chunchaolee <chunchaolee@gmail.com>
2018-03-10 16:12:45 +0800

    setup relational db between interest and user model

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,2 +1,4 @@
 class Interest < ApplicationRecord
+  # 每一筆interst資料只屬於一個user
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # 一個user可以有很多筆interest資料
+  has_many :interests
+
 end
