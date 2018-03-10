e469f5de239c00410a0791e4f4869888cd764bae
chunchaolee <chunchaolee@gmail.com>
2018-03-10 16:16:11 +0800

    setup relational db between interest and event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  # 一個event可以有很多筆被interest的資料
+  has_many :interests
 end
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,4 +1,6 @@
 class Interest < ApplicationRecord
   # 每一筆interst資料只屬於一個user
   belongs_to :user
+  # 每一筆interst資料只對應到一個event
+  belongs_to :event
 end
