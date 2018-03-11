931c42daf720d9125bb9d335255dd17736ea5b14
chunchaolee <chunchaolee@gmail.com>
2018-03-11 14:57:04 +0800

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "events#index"
+
+  # setup events
+  resources :events, only: [:index, :show] do
+
+    # All posts of events ; like index
+    collection do
+      get :posts
+    end
+
+  end
+
+  # setup user
+  resources :users, only: [:show, :edit, :update]
+
+  # setup interest
+  resources :interests, only: [:create, :destroy] 
+
+  # setup admin
+  namespace :admin do
+    root "events#index"
+    resources :events, except: [:show, :new, :create]
+    resources :users, only: [:index, :destroy]
+  end
+
 end
