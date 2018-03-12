b3a9a9c0e07aaf412a893de8ca732f2031911494
chunchaolee <chunchaolee@gmail.com>
2018-03-12 22:54:33 +0800

    setup permit parameter name in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # 加上自訂欄位name到Devise的註冊和編輯頁面
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
 end
