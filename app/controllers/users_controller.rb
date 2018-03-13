1974366def436152d54601f82b252c39b136ed33
chunchaolee <chunchaolee@gmail.com>
2018-03-13 15:07:41 +0800

    setup user edit page, and added methods in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :update]
+
+  def update
+    # user validates
+    if current_user == @user
+      if @user.update_attributes(user_params)
+        redirect_to root_url, :notice => "個人資料更新成功"
+      else
+        render :action => :edit
+      end
+    else
+      redirect_to root_url, :alert => "您非該使用者，無法更變該使用者資料"
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
+
 end
