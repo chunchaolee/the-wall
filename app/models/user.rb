1beb9eb12969daeb0fd2e5068d64381e1b08ce96
LanceRabbit <lancerabbit@gmail.com>
2018-03-13 11:19:44 +0800

    add method for facebook login on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,20 @@ class User < ApplicationRecord
   # 一個user可以收到很多notification來自不同的event
   has_many :notified_events, through: :notifications, source: :event
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] &&  session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.image = auth.info.avatar
+    end
+  end
 end
