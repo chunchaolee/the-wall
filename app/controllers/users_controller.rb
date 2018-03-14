f6060013792882f4b1a2caafac5d97fa1f44fcf3
chunchaolee <chunchaolee@gmail.com>
2018-03-14 13:11:00 +0800

    setup user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show]
 
   def update
     # user validates
