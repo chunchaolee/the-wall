f66b507681d6cc9949a71c281d472a788a5fbcc5
chunchaolee <chunchaolee@gmail.com>
2018-03-18 13:36:23 +0800

    added column provider to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317072931) do
+ActiveRecord::Schema.define(version: 20180318044456) do
 
   create_table "events", force: :cascade do |t|
     t.string "artist_name"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20180317072931) do
     t.string "fb_token"
     t.string "fb_connect"
     t.string "spotify_connect"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid"], name: "index_users_on_uid"
