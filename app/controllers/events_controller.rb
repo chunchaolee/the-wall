01023c139e12f2f8832ddd9a008dcc2ed7b87c41
chunchaolee <chunchaolee@gmail.com>
2018-03-20 17:28:26 +0800

    setup event show action for auto get spotify artist playbook

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,22 @@ class EventsController < ApplicationController
 
   end
 
+  def show
+    @event = Event.find(params[:id])
+    artist_name = @event.artist_name
+
+    if @event.spotify_artist_id == nil
+      @spotify_data = Event.get_spotify_data(artist_name)
+      if @spotify_data == nil
+        @spotify_artist_id = nil
+      else
+        @spotify_artist_id = @spotify_data.id
+        @event.spotify_artist_id = @spotify_artist_id
+        @event.save
+      end
+    else
+      @spotify_artist_id = @event.spotify_artist_id
+    end
+  end
+
 end
