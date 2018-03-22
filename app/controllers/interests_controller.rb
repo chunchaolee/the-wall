72777f80ce692c49e880dbb483177585d10ab8e9
annie chiang <annie100patty@gmail.com>
2018-03-22 16:00:14 +0800

    add create action for interest record

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,2 +1,14 @@
 class InterestsController < ApplicationController
+
+  def create
+    @interest = current_user.interests.build(event_id: params[:event_id])
+
+    if @interest.save
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @interest.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
 end
