d14b82a07ff5d1e3b698cf43d131e816b3f9b476
annie chiang <annie100patty@gmail.com>
2018-03-24 16:11:44 +0800

    version 1 of event post

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,7 +45,7 @@ class EventsController < ApplicationController
   def posts
     # ransack
     @q = Event.ransack(ransack_params)
-    @events = @q.result(distinct: true).order(date: :desc)
+    @events = @q.result(distinct: true).order(created_at: :desc)
     @popular_events = Event.all.order(interests_count: :desc).limit(5)
   end
 
