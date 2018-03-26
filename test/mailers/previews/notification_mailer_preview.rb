7b104eecb4f3cc825c7a539ad4d4e75caddfbbcf
LanceRabbit <lancerabbit@gmail.com>
2018-03-26 23:05:35 +0800

    send mail when users interested event

---
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
+class NotificationMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/event_notification
+  def event_notification
+    NotificationMailer.event_notification
+  end
+
+end
