b0985ccd9cf419e13a46ea4039ee4b39a286d3d1
chunchaolee <chunchaolee@gmail.com>
2018-04-03 13:11:53 +0800

    FIX: migrate error on heroku

--- db/migrate/20180326094732_change_column_time_on_event.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnTimeOnEvent < ActiveRecord::Migration[5.1]
-  def change
-    change_column :events, :time, :timestamptz
-  end
-end
--- db/migrate/20180326095808_change_column_time_datatype_on_event.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnTimeDatatypeOnEvent < ActiveRecord::Migration[5.1]
-  def change
-    change_column :events, :time, :string
-  end
-end
