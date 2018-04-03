17bcfd306c9e59f919b71b9e160302a343ddf43a
chunchaolee <chunchaolee@gmail.com>
2018-04-03 12:03:11 +0800

    setup artist model and added artist_id to artists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326162532) do
+ActiveRecord::Schema.define(version: 20180402015327) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180326162532) do
     t.datetime "updated_at", null: false
     t.string "special_id"
     t.string "spotify_artist_id"
+    t.string "artist_id"
   end
 
   create_table "interests", force: :cascade do |t|
