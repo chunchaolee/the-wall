105cadc917d2858bd32f439bf294fce8a9234a25
chunchaolee <chunchaolee@gmail.com>
2018-04-09 20:32:24 +0800

    removed get_spotify_data method from event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -41,30 +41,6 @@ class Event < ApplicationRecord
     'Taipei': '台北',
   }
 
-
-  def get_spotify_data(artist_name)
-
-    if artist_name != nil
-      require 'rspotify'
-
-      if Rails.env.development?
-        # local用
-        spotify_config = Rails.application.config_for(:spotify)
-        client_id = spotify_config["client_id"]
-        client_secret = spotify_config["client_secret"]
-      elsif Rails.env.production?
-        # heroku用
-        client_id = ENV['SPOTIFY_CLIENT_ID']
-        client_secret = ENV['SPOTIFY_CLIENT__SECRET']
-      end
-
-      RSpotify.authenticate("#{client_id}", "#{client_secret}")
-      artist = RSpotify::Artist.search(artist_name)
-      artist_data = artist.first
-    end
-
-  end
-
   def count_views
     self.views_count += 1
     self.save
